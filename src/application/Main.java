c4152337822afafcd69123afd750e2cd675d600f
luck7777 <ljca84@gmail.com>
2020-08-18 07:16:14 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,6 +10,8 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -25,6 +27,11 @@ public class Main extends Application {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
+		
 	}
 
 	public static void main(String[] args) {
